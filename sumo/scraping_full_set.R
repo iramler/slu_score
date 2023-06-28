dcbc3ac007abcacc657298bab597e1c4b86ae28c
jackfay7 <jfay2770@gmail.com>
2023-06-28 10:03:37 -0400

    natsu tournament scraping

--- sumo/scraping_full_set.R
+++ sumo/scraping_full_set.R
@@ -1,20 +1,18 @@
 library(tidyverse)
 library(rvest)
 
-# need to find a way so i dont have to change the basho number everytime 
-# but its 01, not 1, and goes to 11. Another issue is they go up by 2
 
 
-# currently best idea is to have one function for the first 5 then one for the last because it changes to 11
 
-
-# 01-09
-
-sumo_function <- function(i, year = 2019:2023){
+# Scraping data from the top division for all Natsu bashos in history
+# Division: Makuuchi Banzuke
+# Starting in 1757
+# Ending in 2023
+sumo_function <- function(i){
   
   base_url <- "https://sumodb.sumogames.de/Banzuke.aspx?b="
   
-  end_url <- "05&heya=-1&shusshin=-1&bd=on&w=on&hl=on"
+  end_url <- "05&heya=-1&shusshin=-1&h=on&sh=on&bd=on&w=on&hl=on&c=on"
   
   
   url <- paste0(base_url, i, end_url)
@@ -29,18 +27,29 @@ X <- objs[[6]]
 }
 
 
-sumo_2023 <- sumo_function(i)
-sumo_2023<-
-  sumo_2023%>%
-  as.data.frame()
+natsu_full <- sumo_function(i = 2023)
 
 
-for(i in 2019:2022){
+
+
+for(i in 1757:2022){
   Sys.sleep(rpois(1, 3)+1)
   additional_rows <- sumo_function(i)
   
-  additional_rows<- additional_rows%>%
-    as.data.frame()
   
-  natsu_2023 <- bind_rows("2023" = natsu_2023, "2022" = additional_rows, .id = "year")
+  natsu_full <- bind_rows(natsu_full, additional_rows)
 }
+
+
+
+
+
+
+
+# scrape first one 
+# change 2nd result name 
+# do same for the rest in for loop 
+# bind rows to original in the loop 
+# clean after
+
+# to get all tournaments do for loop inside for loop (with exception to 11)
